aa877e1adfb625777f526fc5f622e20f3604627a
Rakeen Huq <rakeen.huq@redthreadinnovations.com>
2022-08-01 16:51:10 -0400

    just installed react app in "client" and create react app in "create" as two alternate frontends for now

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Where we will keep books
 let books = [];
