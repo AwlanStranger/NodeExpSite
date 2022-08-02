b0ccd4209057203fc0e9d0138451863149739b0d
Rakeen Huq <rakeen.huq@redthreadinnovations.com>
2022-08-01 21:26:39 -0400

    changed default cra template to solar event page

--- create/src/App.js
+++ create/src/App.js
@@ -3,21 +3,48 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <hr/>
+      <h1>Where is the Sun?</h1>
+      <hr/>
+
+      <form action="http://localhost:3001/book" method="POST">
+        
+          {/* Timezone selector */}
+          <div class="form-group">
+              <label for="Timezone">Timezone</label>
+              <select class="form-control" name="timezone">
+                  <option value="America/Toronto">EDT</option>
+                  <option value="America/Panama">EST</option>
+                  <option value="Africa/Abidjan">UTC</option>
+              </select>
+          </div>
+
+          {/* Event selector */}
+          <div class="form-group">
+              <label for="Event">Event</label>
+              <select class="form-control" name="event">
+                  <option value="sunrise">Sunrise</option>
+                  <option value="solarNoon">Solar Noon</option>
+                  <option value="sunset">Sunset</option>
+              </select>
+          </div>
+
+          {/* Latitude input */}
+          <div class="form-group">
+              <label for="Latitude">Latitude</label>
+              <input class="form-control" type="number" step=".000001" name="latitude"/>
+          </div>
+
+          {/* Longitude input */}
+          <div class="form-group">
+              <label for="Longitude">Longitude</label>
+              <input class="form-control" type="number" step=".000001" name="longitude"/>
+          </div>
+
+          {/* Submit button */}
+          <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
     </div>
   );
 }
