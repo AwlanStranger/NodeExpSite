b93853333c924b90a826a0cd3f4904c8109fdc38
Rakeen Huq <rakeen.huq@redthreadinnovations.com>
2022-10-29 18:10:41 -0400

    Changed solar event functions to return utc time instead of local time strings to allow them to be used in calculations also added functionality for fajr.

--- prayerTimes.js
+++ prayerTimes.js
@@ -1,22 +1,27 @@
 const SunCalc = require('suncalc');
 const DateFnsTz = require('date-fns-tz');
 
-function convertTZ(date, tzString) {
-    return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));   
-}
+// function convertTZ(date, tzString) {
+//     return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));   
+// }
+
+// THE FOLLOWING FUNCTIONS RETURN A DATE OBJECT IN UTC TIME.
+// THEY ARE USED FOR CALCULATIONS AND SO THE VALUES ARE NOT FOR HUMAN READING.
 
 function getSolarNoon(lat, long, timezone) {
     const utcTimes = SunCalc.getTimes(new Date(), lat, long);
     const utcTime = utcTimes.solarNoon;
     const localTime = utcTime.toLocaleTimeString("en-US", {timeZone: timezone, hour12: false})
-    return localTime;
+    // return localTime;
+    return utcTime;
 }
 
 function getSunrise(lat, long, timezone) {
     const utcTimes = SunCalc.getTimes(new Date(), lat, long);
     const utcTime = utcTimes.sunrise;
     const localTime = utcTime.toLocaleTimeString("en-US", {timeZone: timezone, hour12: false})
-    return localTime;
+    // return localTime;
+    return utcTime;
 }
 
 function getSunset(lat, long, timezone) {
@@ -39,8 +44,26 @@ function getSunset(lat, long, timezone) {
     const utcTimes = SunCalc.getTimes(new Date(), lat, long);
     const utcTime = utcTimes.sunset;
     const localTime = utcTime.toLocaleTimeString("en-US", {timeZone: timezone, hour12: false})
-    return localTime;
+    // return localTime;
+    return utcTime;
 }
+
+function getNightEnd(lat, long, timezone) { // astronomical twilight begins
+    const utcTimes = SunCalc.getTimes(new Date(), lat, long);
+    const utcTime = utcTimes.nightEnd;
+    const localTime = utcTime.toLocaleTimeString("en-US", {timeZone: timezone, hour12: false})
+    // return localTime;
+    return utcTime;
+}
+
+function getNauticalDawn(lat, long, timezone) { // nautical twilight begins
+    const utcTimes = SunCalc.getTimes(new Date(), lat, long);
+    const utcTime = utcTimes.nauticalDawn;
+    const localTime = utcTime.toLocaleTimeString("en-US", {timeZone: timezone, hour12: false})
+    // return localTime;
+    return utcTime;
+}
+
 
 // Toronto Coords
 // lat = 43.653225
@@ -50,9 +73,34 @@ function getSunset(lat, long, timezone) {
 // 51.5072
 // 0.1276
 
+// THESE FUNCTIONS RETURN A STRING INTENDED FOR READING BY HUMANS
+
+// Astronomical Dawn = nightEnd = 18 degrees
+// Nautical Dawn = nauticalDawn
+
+function getFajr(lat, long, timezone) {
+    const astronomicalTwilight = getNightEnd(lat, long, timezone);
+    // console.log(astronomicalTwilight.toLocaleTimeString("en-US", {timeZone: timezone, hour12: false}))
+
+    const nauticalTwilight = getNauticalDawn(lat, long, timezone);
+    // console.log(nauticalTwilight.toLocaleTimeString("en-US", {timeZone: timezone, hour12: false}))
+
+    const astronomicalTwilightMs = astronomicalTwilight.getTime();
+    const nauticalTwilightMs = nauticalTwilight.getTime();
+    const midpointMs = Math.round((nauticalTwilightMs - astronomicalTwilightMs)/2);
+    
+    const retval = new Date(astronomicalTwilightMs + midpointMs);
+    return retval.toLocaleTimeString("en-US", {timeZone: timezone, hour12: false})
+}
+
+function getDhuhr(lat, long, timezone) {
+    
+}
+
 // mymodule.js
 module.exports = {
     getSolarNoon,
     getSunrise,
     getSunset,
+    getFajr
 }
--- test.js
+++ test.js
@@ -1,8 +1,3 @@
-const DateFnsTz = require('date-fns-tz');
+const prayerTimes = require('./prayerTimes');
 
-const date = DateFnsTz.getDatePickerValue() // e.g. 2014-06-25 10:00:00 (picked in any time zone)
-const timeZone = DateFnsTz.getTimeZoneValue() // e.g. America/Los_Angeles
-
-const utcDate = DateFnsTz.zonedTimeToUtc(date, timeZone) // In June 10am in Los Angeles is 5pm UTC
-
-postToServer(utcDate.toISOString(), timeZone) // post 2014-06-25T17:00:00.000Z, America/Los_Angeles
+console.log(prayerTimes.getFajr(43, -79, 'America/Toronto'));
