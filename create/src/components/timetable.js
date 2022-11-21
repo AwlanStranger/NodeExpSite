d292d6dedadc55385b36e9b0550d6da478c6bbb0
Rakeen Huq <rakeen.huq@redthreadinnovations.com>
2022-11-21 14:54:02 -0500

    timetable component now prints just lat and long of location

--- create/src/components/timetable.js
+++ create/src/components/timetable.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
+// import { render } from 'react-dom'
 
 const DEFAULT_TIME = 'Please fill out the form';
 
-export default function Timetable(params) {
+export default function Output(params) {
+
+  // const [state, setState] = useState({
+  //     // Setting default values for state variables
+  //     time: DEFAULT_TIME
+  // })
+
+  function formSubmit(event) {
+      event.preventDefault();
+  }
+
 
   return (
-    <div class="container">
-      Latitude: {params.lat}<br/>
-      Longitude: {params.long}
-    </div>
-  );
+  <div class="container">
+    Lat: {params.lat}<br/>
+    Long: {params.long}
+  </div>
+);
 }
