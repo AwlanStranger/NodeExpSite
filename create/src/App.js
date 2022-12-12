aecda32ce3cfb732797cf56da4c3455926cd8094
Rakeen Huq <rakeen.huq@redthreadinnovations.com>
2022-12-12 14:02:14 -0500

    Added leaflet map. currently not functional.

--- create/src/App.js
+++ create/src/App.js
@@ -6,6 +6,8 @@ import Output from './components/output';
 import Location from './components/locationSelector'
 import Timetable from './components/timetable'
 
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+
 const DEFAULT_TIME = 'Please fill out the form';
 const DEFAULT_LAT = 0;
 const DEFAULT_LONG = 0;
@@ -39,10 +41,19 @@ function App() {
 
   return (
     <div>
+
       <SolarEvent updateSolarState={updateSolarState}/>
       <Output time={state.time}/>
       <Location updateLocation={updateLocation}/>
       <Timetable lat={state.lat} long={state.long}/>
+
+      {/* <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer> */}
+
     </div>
   );
 }
